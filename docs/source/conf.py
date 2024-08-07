16577f0a2b50337607f8806b615a4f281afd6104
Prakash, Nina <Nina.Prakash@nrel.gov>
2024-08-07 17:17:24 -0600

    Update readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Paul Gasper, Kandler Smith'
 author = 'Paul Gasper, Kandler Smith'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.2'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
