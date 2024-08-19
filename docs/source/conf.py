213038e2a2e9b5655ff7b8c12af3ef5d22925b19
Prakash, Nina <Nina.Prakash@nrel.gov>
2024-08-19 08:35:40 -0600

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Paul Gasper, Kandler Smith'
 author = 'Paul Gasper, Kandler Smith'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
