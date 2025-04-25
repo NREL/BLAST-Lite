Fix temperature normalization in J. Energy Stor. paper models

blast/models/nmc_gr_50Ah_B1_2020.py
@@ -114,7 +114,7 @@ class NMC_Gr_50Ah_B1(BatteryDegradationModel):
         Ua = stressors["Ua"]
         dod = stressors["dod"]
         Crate = stressors["Crate"]
-        TdegKN = TdegK / (293.15 + 35)
+        TdegKN = TdegK / (273.15 + 35)
         UaN = Ua / 0.123 # Ua at about 50% SOC
 
         # Grab parameters
@@ -124,8 +124,11 @@ class NMC_Gr_50Ah_B1(BatteryDegradationModel):
         kcal = (p['p1']
             * np.exp(p['p2']*(1/(TdegKN**3)) * (UaN**(1/3))))
 
-        kcyc = np.abs(p['p3']
-                * ((dod**2)*(TdegKN**3)*(Crate**0.5))**p['p4'])
+        kcyc = np.abs(
+            p['p3']
+            * (
+                (dod**2)*(TdegKN**3)*(Crate**0.5)
+              )**p['p4'])
         
         # Calculate time based average of each rate
         kcal = np.trapz(kcal, x=t_secs) / delta_t_secs

blast/models/nmc_gr_75Ah_A_2019.py
@@ -114,7 +114,7 @@ class NMC_Gr_75Ah_A(BatteryDegradationModel):
         Ua = stressors["Ua"]
         dod = stressors["dod"]
         Crate = stressors["Crate"]
-        TdegKN = TdegK / (293.15 + 35)
+        TdegKN = TdegK / (273.15 + 35)
         UaN = Ua / 0.123 # Ua at about 50% SOC
 
         # Grab parameters
@@ -123,9 +123,12 @@ class NMC_Gr_75Ah_A(BatteryDegradationModel):
         # Calculate the degradation coefficients
         kcal = (p['p1']
             * np.exp(p['p2']*(TdegKN**3) * (1/(UaN**(1/3)))))
-        kcyc = np.abs(p['p3'] + p['p4']*(TdegKN**3)*(dod**0.5) 
-        + p['p5']*np.exp((1/(TdegKN**0.5)) * (Crate**2))
-        + p['p6']*np.exp((dod**0.5) * (TdegKN**2) * (Crate**0.5)))
+        kcyc = np.abs(
+            p['p3'] 
+            + p['p4']*(TdegKN**3)*(dod**0.5) 
+            + p['p5']*np.exp((1/(TdegKN**0.5)) * (Crate**2))
+            + p['p6']*np.exp((dod**0.5) * (TdegKN**2) * (Crate**0.5))
+            )
         
         # Calculate time based average of each rate
         kcal = np.trapz(kcal, x=t_secs) / delta_t_secs